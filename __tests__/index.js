d6d86a784e8fb6d54f521f976fb6e892b7a26b57
mortalYoung <yangwei1@outlook.com>
2023-04-20 23:17:29 +0800

    update

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,4 +1,5 @@
 const M = require("../lib/index.js");
+const Mock = require("../lib/utils.js");
 
 const mock = new M(
   {
@@ -9,23 +10,26 @@ const mock = new M(
         data: {
           currentPage: params.currentPage,
           pageSize: params.pageSize,
-          data: new Array(10).fill(1).map((_, idx) => {
-            const projectName = M.MockProject();
-            return {
-              id: idx,
-              idxTotal: M.MockNumber({ min: 0, max: 20, precision: 1 }),
-              isSpace: M.MockBoolean(),
-              projectName,
-              createUser: "admin@dtstack.com",
-              gmtCreate: new Date().valueOf(),
-              projectShowName: `${projectName}`,
-              modelTotal: M.MockNumber({ min: 1, max: 5, precision: 1 }),
-              publishIdx: M.MockNumber({ min: 1, max: 200, precision: 1 }),
-              status: M.MockNumber({ min: 0, max: 1, precision: 1 }),
-              topSign: 0,
-            };
-          }),
-          totalCount: 10,
+          data: Mock.MockPagination("1")
+            .length(50)
+            .fill((idx) => {
+              const projectName = Mock.MockProject();
+              return {
+                id: idx,
+                idxTotal: Mock.MockNumber({ min: 0, max: 20, precision: 1 }),
+                isSpace: Mock.MockBoolean(),
+                projectName,
+                createUser: "admin@dtstack.com",
+                gmtCreate: new Date().valueOf(),
+                projectShowName: `${projectName}`,
+                modelTotal: Mock.MockNumber({ min: 1, max: 5, precision: 1 }),
+                publishIdx: Mock.MockNumber({ min: 1, max: 200, precision: 1 }),
+                status: Mock.MockNumber({ min: 0, max: 1, precision: 1 }),
+                topSign: 0,
+              };
+            })
+            .set(params.currentPage, params.pageSize),
+          totalCount: 50,
           totalPage: 1,
         },
         space: 0,

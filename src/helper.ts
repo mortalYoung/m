39153d393a87bf76c8fb08a54e37cb4617d24102
野迂迂 <yangwei1@outlook.com>
2023-05-11 20:33:13 +0800

    improve MockPagination

--- src/helper.ts
+++ src/helper.ts
@@ -4,41 +4,35 @@ faker.setLocale("zh_CN");
 
 const cache = new Map();
 
+interface IMockPagination {
+	key?: string;
+	total: number;
+	current: number;
+	pageSize?: number;
+	dataType: (idx: number) => any;
+}
+
 class Mock {
 	static MockId = () => faker.datatype.uuid();
 	static MockNumber = ({ min, max, precision }: Partial<{ min: number; max: number; precision: number }>) =>
 		faker.datatype.number({ max, min, precision });
 	static MockBoolean = () => faker.datatype.boolean();
-	static MockProject = () => faker.word.noun();
-	static MockPagination = (hash: string) => {
-		if (cache.has(hash)) {
-			return {
-				length: () => ({
-					fill: () => ({
-						set: (current: number, pageSize: number) => {
-							const data = cache.get(hash);
-							const offset = (current - 1) * pageSize;
-							return data.slice(offset, offset + pageSize);
-						},
-					}),
-				}),
-			};
+
+	static MockProject = () => faker.commerce.product();
+	static MockPagination = (rawOp: IMockPagination) => {
+		const op: Required<IMockPagination> = { key: new Date().valueOf().toString(), pageSize: 20, ...rawOp };
+		const { key, current, pageSize, dataType } = op;
+		if (cache.has(key)) {
+			const data = cache.get(key);
+			const offset = (current - 1) * pageSize;
+			return data.slice(offset, offset + pageSize);
 		}
 
-		return {
-			length: (length: number) => ({
-				fill: (func: (index: number) => any) => {
-					const data = new Array(length).fill(1).map((_, idx) => func(idx));
-					cache.set(hash, data);
-					return {
-						set: (current: number, pageSize: number) => {
-							const offset = (current - 1) * pageSize;
-							return data.slice(offset, offset + pageSize);
-						},
-					};
-				},
-			}),
-		};
+		const data = new Array(length).fill(1).map((_, idx) => dataType(idx));
+		cache.set(key, data);
+
+		const offset = (current - 1) * pageSize;
+		return data.slice(offset, offset + pageSize);
 	};
 }
 
